5282ddf7a784dac83092937366c1d6d8a1529e2f
prosenjit005 <115132481+EasyCodePro@users.noreply.github.com>
2022-10-06 14:15:36 +0530

    initial commit

---
+++ Parallel_API_Calls/Parallel_API_Calls/src/main/java/com/ecp/parallelapicalls/ParallelApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.ecp.parallelapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelApiCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelApiCallsApplication.class, args);
+	}
+
+}
